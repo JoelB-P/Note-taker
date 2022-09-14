e9ce3917a8fbd26c9dbbbe99b9bb66d2de91e20c
Your Name <joelebell@gmail.com>
2022-09-14 15:43:15 -0400

    Final

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const {uuid} = require('uuidv4');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,17 +11,32 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
 
 
-// HTML routes
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
-});
-
-app.get('/notes', (req,res) => {
+// HTML route
+app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'))
 });
 
+// API routes
+app.get('/api/notes',(req, res) => {
+    let notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+    res.json(notes)
+});
+
+app.post('/api/notes', (req, res) => {
+    let notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+    let newNote = req.body;
+    newNote.id = uuid();
+    let updatedNotes = [...notes, newNote];
+    fs.writeFileSync('./db/db.json', JSON.stringify(updatedNotes));
+    res.json(updatedNotes);
+})
 
 
+
+// HTML route
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+});
 
 
 
